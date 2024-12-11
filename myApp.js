b2654272e3b3184a4dd365bf83cb5025556984fc
Yash doe <yasgavande78@gmail.com>
2024-12-11 16:30:52 +0000

    test

--- myApp.js
+++ myApp.js
@@ -1,5 +1,26 @@
+require('dotenv').config()
 let express = require('express');
+const res = require('express/lib/response');
 let app = express();
+console.log("Hello World");
+// app.get("/",(req,res)=>{
+//     res.send("Hello Express")
+// })
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/views/index.html")
+})
+app.use("/public", express.static(__dirname + "/public"))
+
+var message= 'Hello json';
+
+app.get("/json", (req, res) => {
+  if ( process.env['MESSAGE_STYLE'] === "uppercase") {
+      res.json({ "message": message.toUpperCase() });
+  }
+  else {
+    res.json({ "message": message });
+  }
+});
 
 
 
@@ -32,8 +53,4 @@ let app = express();
 
 
 
-
-
-
-
- module.exports = app;
+module.exports = app;
